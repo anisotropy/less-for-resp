31481a853ec2e4a2ff8b8987f173c7781d61ed6d
hodoug <j.hodoug@gmail.com>
2017-08-10 12:26:47 +0900

    First commit

---
+++ example/dev/src/App.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import './style/style.less';
+
+render(
+	<div className="box">
+	</div>,
+	document.getElementById('root')
+);
